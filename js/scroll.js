152a7a96d7cca021e344f2996bba672a3676fb19
agrochal <artur@procer.pl>
2020-06-30 18:06:31 +0200

    Added footer

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,17 @@
 const scroll = document.getElementById("scroll");
+const panel = document.getElementById("projects_list");
 
-scroll.oninput = function () {
-	const panel = document.getElementById("projects_list");
+scroll.oninput = function() {
 
-	let total = panel.scrollWidth - panel.offsetWidth;
-	let percentage = total*(this.value/100);
+  let total = panel.scrollWidth - panel.offsetWidth;
+  let percentage = total * (this.value / 100);
 
-	panel.scrollLeft = percentage;
+  panel.scrollLeft = percentage;
+}
+
+panel.onscroll = function() {
+  let total = panel.scrollWidth - panel.offsetWidth;
+  let percentage = panel.scrollLeft / total * 100;
+  scroll.value = percentage;
+
 }

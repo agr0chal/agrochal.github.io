211200368b3c7a441420988ef794e10e3138ff8f
agrochal <artur@procer.pl>
2020-07-02 12:04:50 +0200

    Fixed extension issue

--- js/projects.js
+++ js/projects.js
@@ -20,7 +20,7 @@ async function getProjects() {
 
     projects += `
       <div class="project">
-        <img src="./img/${image}.PNG" alt="logo "${image}">
+        <img src="./img/${image}.png" alt="logo "${image}">
         <div class="data_box">
           <h2>${name}</h2>
           <h3>${desc}</h3>
